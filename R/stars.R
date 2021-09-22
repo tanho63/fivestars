20a3cfda8eb484ca3a3b3af3741fb31836b30332
Tan-DESKPC <tan@tanho.ca>
2021-09-22 10:21:15 -0400

    initial commit

---
+++ R/stars.R
@@ -0,0 +1,101 @@
+#' Add a star
+#' 
+#' This function creates an HTML tag with the appropriate number of stars, as well as an accessibility label.
+#'
+#' @param rating rating out of five
+#' @param text text to display in tag, defaults to the rating rounded to one decimal place
+#' @param aria_label accessibility label for text, defaults to "rating 
+#' @param star_size CSS font-size for star background, default is to inherit from `use_fivestars()`
+#' @param star_color CSS colour for stars, default is to inherit from `use_fivestars()`
+#' @param star_background CSS colour for star background, default is to inherit from `use_fivestars()`
+#' 
+#' @examples 
+#' fivestar(3.5)
+#' 
+#' @return an html tag that renders a star rating out of five 
+#' @export
+fivestar <- function(rating,
+                     text = round(rating,1),
+                     aria_label = paste("The rating out of five stars is", text),
+                     star_size = NULL,
+                     star_color = NULL,
+                     star_background = NULL){
+  
+  stopifnot(
+    is.numeric(rating),
+    !is.na(rating))
+  
+  style_args <- list(`--rating` = rating, 
+                     `--star_size` = star_size, 
+                     `--star_color` = star_color, 
+                     `--star_background` = star_background)
+  
+  style_args <- drop_nulls(style_args)
+  
+  style <- paste0(paste(names(style_args),style_args,sep = ": ",collapse = "; "),";")
+  
+  htmltools::tags$span(
+    text,
+    class = "fivestars",
+    style = style,
+    `aria-label` = aria_label
+  )
+}
+
+#' Add stars CSS
+#'
+#' This function adds the appropriate CSS to your page for star ratings.
+#' 
+#' @param star_size CSS font size for your stars, default is `"larger"`
+#' @param star_color CSS colour for star, default is `#fc0`
+#' @param star_background CSS colour for star background, default is `#999`
+#' 
+#' @examples 
+#' use_fivestars()
+#' 
+#' @return HTML that inserts CSS into the page head
+#' @export
+use_fivestars <- function(star_size = "larger", star_color = "#fc0", star_background = "#999"){
+  
+  stopifnot(
+    !is.na(star_size),
+    !is.na(star_color),
+    !is.na(star_background),
+    is.character(star_size),
+    is.character(star_color),
+    is.character(star_background)
+  )
+  
+  htmltools::tags$head(
+    htmltools::tags$style(
+      htmltools::HTML(
+        glue::glue("
+          :root {
+            --star-size: {{star_size}};
+            --star-background: {{star_background}};
+            --star-color: {{star_color}};
+          }
+        
+          .fivestars {
+            --percent: calc(var(--rating)/5 * 100%);
+            /* display: inline-block; */
+            font-size: var(--star-size);
+            line-height: 1;
+          }
+          
+          .fivestars::before {
+            content: '\\2605\\2605\\2605\\2605\\2605';
+            background: linear-gradient(90deg, var(--star-color) var(--percent), 
+                                        var(--star-background) var(--percent));
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+          }
+          ",
+          .open = "{{",
+          .close = "}}"
+        )
+      )
+    )
+  )
+  
+}
---
+++ README.md
@@ -0,0 +1,55 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# fivestars
+
+<!-- badges: start -->
+<!-- [![CRAN status](https://img.shields.io/cran/v/fivestars?style=flat-square&logo=R&label=CRAN)](https://CRAN.R-project.org/package=fivestars)  -->
+
+[![Dev
+status](https://img.shields.io/github/r-package/v/tanho63/fivestars/main?label=dev%20version&style=flat-square&logo=github)](https://fivestars.dynastyprocess.com/)
+[![Lifecycle:
+experimental](https://img.shields.io/badge/lifecycle-experimental-orange.svg?style=flat-square)](https://lifecycle.r-lib.org/articles/stages.html)
+[![R build
+status](https://img.shields.io/github/workflow/status/tanho63/fivestars/R-CMD-check?label=R%20check&style=flat-square&logo=github)](https://github.com/tanho63/fivestars/actions)
+[![Codecov test
+coverage](https://img.shields.io/codecov/c/github/tanho63/fivestars?label=codecov&style=flat-square&logo=codecov)](https://codecov.io/gh/tanho63/fivestars?branch=main)
+
+<!-- badges: end -->
+
+The goal of {fivestars} is to generate star ratings with HTML and CSS,
+primarily for Shiny usage.
+
+## Installation
+
+Install the development version from GitHub with:
+
+``` r
+install.packages("fivestars", repos = "https://tanho63.r-universe.dev")
+
+# or use remotes/devtools
+# install.packages("remotes")
+remotes::install_github("tanho63/fivestars")
+```
+
+## Usage
+
+``` r
+library(shiny)
+library(fivestars)
+
+ui <- fluidPage(
+  use_fivestars(),
+  fivestar(2.2)
+)
+
+server <- function(input, output, session) {
+  
+}
+
+shinyApp(ui, server)
+#> 
+#> Listening on http://127.0.0.1:5018
+```
+
+<img src="man/figures/README-unnamed-chunk-3-1.png" width="100%" />
